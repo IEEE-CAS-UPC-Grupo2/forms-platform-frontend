0bc6df0d970b55726dc61fb83a4f056b02f81647
Cristina <avicon95@protonmail.com>
2024-05-12 18:57:16 -0500

    chore: form space before form element

--- app/[admin_token]/page.tsx
+++ app/[admin_token]/page.tsx
@@ -1,17 +1,16 @@
 import { CustomButton } from "../components/CustomButton";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faShareFromSquare } from "@fortawesome/free-solid-svg-icons";
 
 export default function Page() {
   return (
     <main className="flex flex-col justify-center items-center h-screen">
-      <h1>Admin Login Page</h1>
-      <CustomButton>Ingresar</CustomButton>
-      <br />
-      <CustomButton>
-        <FontAwesomeIcon className="pr-2" icon={faShareFromSquare} />
-        Comparte el evento
-      </CustomButton>
+      <div className="bg-cas-gray-light p-20 flex flex-col justify-center items-center">
+        <h1>Login de Administrador</h1>
+        <div className="flex flex-col w-full my-10">
+          <small>Correo Electrónico</small>
+          <small>Contraseña</small>
+        </div>
+        <CustomButton>Ingresar</CustomButton>
+      </div>
     </main>
   );
 }
--- app/event/[event_id]/register/page.tsx
+++ app/event/[event_id]/register/page.tsx
@@ -1,8 +1,16 @@
+import { CustomButton } from "@/app/components/CustomButton";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShareFromSquare } from "@fortawesome/free-solid-svg-icons";
+
 export default function Page({ params }: { params: { event_id: string } }) {
   return (
-    <main>
+    <main className="pt-20">
       <h1>Registered Events Page</h1>
       <div>Event id: {params.event_id}</div>
+      <CustomButton>
+        <FontAwesomeIcon className="pr-2" icon={faShareFromSquare} />
+        Comparte el evento
+      </CustomButton>
     </main>
   );
 }
