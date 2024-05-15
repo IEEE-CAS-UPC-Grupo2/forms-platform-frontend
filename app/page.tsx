1a6c85707c966969dad3673c49ad31e68b132358
Cristina <avicon95@protonmail.com>
2024-05-14 19:51:27 -0500

    chore: create upper banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
+import { EventCard } from "./components/EventCard";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Events Page</h1>
+    <main className="flex min-h-screen flex-col pt-20">
+      <div className="bg-cas-green flex flex-col justify-center text-center w-full text-cas-white p-12 shadow-cas-gray shadow-sm">
+        <h1 className="mb-4">Próximos Eventos</h1>
+        <p>
+          Descubre eventos próximos: conferencias, talleres y más. ¡Asegura tu
+          lugar hoy mismo!
+        </p>
+      </div>
+      <div className="p-4 sm:py-10 sm:px-12 md:px-20 xl:px-40">
+        <div>SearchBar</div>
+        <EventCard />
+      </div>
     </main>
   );
 }
