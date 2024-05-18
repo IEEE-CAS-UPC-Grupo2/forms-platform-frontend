045a7235bf7a6efb0e01e2c2a72d2aa215424d4f
Cristina <avicon95@protonmail.com>
2024-05-18 11:32:41 -0500

    Images loading animation

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -1,11 +1,22 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FC } from "react";
 
-export const SearchBar = () => {
+interface SearchBarProps {
+  searchQuery: string;
+  setSearchQuery: (query: string) => void;
+}
+
+export const SearchBar: FC<SearchBarProps> = ({
+  searchQuery,
+  setSearchQuery,
+}) => {
   return (
     <div className="hidden md:flex h-18 pb-10 text-cas-gray-mid">
       <input
         className="h-full w-full p-2 border-cas-gray-mid border-[1.6px] rounded-s focus:outline-none"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
         placeholder="Buscar un evento..."
       />
       <button className="h-18 bg-cas-gray-mid rounded-e text-cas-white w-10">
--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,11 @@ import { useEffect, useState } from "react";
 import { Event } from "./api/events/data";
 import { EventCard } from "./components/EventCard";
 import { SearchBar } from "./components/SearchBar";
+import { formatEventDate } from "./utils/formatDate";
 
 export default function Home() {
   const [events, setEvents] = useState<Event[]>([]);
+  const [searchQuery, setSearchQuery] = useState<string>("");
 
   const getEvents = async () => {
     try {
@@ -23,8 +25,15 @@ export default function Home() {
 
   const today = new Date();
 
-  const sortedEvents = events
+  const filteredEvents = events
     .filter((event) => new Date(event.EventDateAndTime) >= today)
+    .filter(
+      (event) =>
+        event.EventTitle.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        formatEventDate(event.EventDateAndTime)
+          .toLowerCase()
+          .includes(searchQuery.toLowerCase()),
+    )
     .sort(
       (a, b) =>
         new Date(a.EventDateAndTime).getTime() -
@@ -41,10 +50,10 @@ export default function Home() {
         </p>
       </div>
       <div className="px-4 py-10 sm:py-10 sm:px-10 md:px-20 xl:px-32 4xl:px-48">
-        <SearchBar />
-        {sortedEvents.length > 0 ? (
+        <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+        {filteredEvents.length > 0 ? (
           <div className="grid place-content-center justify-items-center grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 4xl:grid-cols-5 gap-8">
-            {sortedEvents.map((event, index) => (
+            {filteredEvents.map((event, index) => (
               <EventCard
                 key={index}
                 title={event.EventTitle}
@@ -54,7 +63,26 @@ export default function Home() {
             ))}
           </div>
         ) : (
-          <p>No events available.</p>
+          <svg
+            className="animate-spin text-cas-green w-20 h-20 mx-auto text-center mt-20"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <circle
+              className="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              stroke-width="4"
+            ></circle>
+            <path
+              className="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            ></path>
+          </svg>
         )}
       </div>
     </main>
