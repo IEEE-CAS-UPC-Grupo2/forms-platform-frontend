2db3e6d31c4dfc6260e884d956d3481e01eb47ea
Eric Cuevas <99770864+Ericcuevas03@users.noreply.github.com>
2024-05-26 16:35:45 -0500

    Update page.tsx

--- app/[admin_token]/panel/page.tsx
+++ app/[admin_token]/panel/page.tsx
@@ -1,7 +1,36 @@
+"use client";
+import { useEffect, useState } from "react";
+import { Event } from "@/app/api/events/data";
+
+import { AdminEventTable } from "@/app/components/AdminEventTable";
 export default function Page() {
+
+  const [events, setEvents] = useState<Event[]>([]);
+
+  const getEvents = async () => {
+    try {
+      const response = await fetch("/api/events");
+      const data = await response.json();
+      setEvents(data.data);
+    } catch (error) {
+      console.error("Error fetching events: ", error);
+    }
+  };
+
+  useEffect(() => {
+    getEvents();
+  }, []);
+
+
   return (
-    <main>
-      <h1 className="fixed top-1/2">Admin Panel Page</h1>
+    <main className="flex flex-col justify-center items-center min-h-screen ">
+      
+      <h1 className="mb-4">Admin Panel Page</h1>
+
+      <div className="w-full max-w-6xl">
+        <AdminEventTable eventos={events} />
+      </div>
+
     </main>
   );
 }
