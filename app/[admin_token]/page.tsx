e814d955b157b0c9c1ca454af2325897efc9549c
Eric Cuevas <99770864+Ericcuevas03@users.noreply.github.com>
2024-06-02 23:07:11 -0500

    Update page.tsx

--- app/[admin_token]/page.tsx
+++ app/[admin_token]/page.tsx
@@ -1,45 +1,10 @@
 "use client";
 
-import { adminSchema } from "@/validations/adminSchema";
-import { CustomButton } from "../components/CustomButton";
-import { Formik, Field, Form, ErrorMessage } from "formik";
-import { FormEntry } from "../components/FormEntry";
+import { AdminLogin } from "@/app/components/AdminLoginPage";
 
 export default function Page() {
+
   return (
-    <main className="flex flex-col justify-center items-center h-screen">
-      <div className="bg-cas-gray-light p-10 sm:p-20 flex flex-col justify-center items-center rounded shadow-cas-gray-light drop-shadow">
-        <h1 className="text-center">Login de Administrador</h1>
-        <div className="flex flex-col w-full mt-10">
-          <Formik
-            initialValues={{
-              email: "",
-              password: "",
-            }}
-            onSubmit={(values) => {
-              // Handle form submission
-              console.log(values);
-            }}
-            validationSchema={adminSchema}
-          >
-            <Form>
-              <FormEntry
-                title={"Correo Electrónico"}
-                name={"email"}
-                type="email"
-              />
-              <FormEntry
-                title={"Contraseña"}
-                name={"password"}
-                type="password"
-              />
-              <div className="mt-4 flex justify-center">
-                <CustomButton type="submit">Ingresar</CustomButton>
-              </div>
-            </Form>
-          </Formik>
-        </div>
-      </div>
-    </main>
+    <AdminLogin />
   );
 }
