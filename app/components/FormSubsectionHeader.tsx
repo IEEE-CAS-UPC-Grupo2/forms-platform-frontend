a30c58fabe9b4d12757ed79292c15827402c306d
Gianpaul Bernal <gianpaulbernalsaravia@gmail.com>
2024-11-30 17:49:40 -0500

    draft: en proceso la subida de archivos

--- app/components/FormSubsectionHeader.tsx
+++ app/components/FormSubsectionHeader.tsx
@@ -2,6 +2,9 @@ import { Formik, Field, Form, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import { Subsection, UpdateSubsectionHeader } from "../models";
 import { updateSubsectionById } from "../api/subsection";
+import { useState } from "react";
+import { uploadImage } from "../api/images-api";
+import Image from "next/image";
 
 interface FormSubsectionHeaderProps {
   id: string;
@@ -12,6 +15,10 @@ export const FormSubsectionHeader = ({
   id,
   subsection,
 }: FormSubsectionHeaderProps) => {
+  const [imageFile, setImageFile] = useState<File | null>(null);
+  const [previewUrl, setPreviewUrl] = useState("");
+  const defaultImage = "https://via.placeholder.com/1920x300?text=Imagen+por+defecto";
+
   const initialValues = {
     title: subsection?.title ?? "",
     description: subsection?.description ?? "",
@@ -21,12 +28,37 @@ export const FormSubsectionHeader = ({
 
   const validationSchema = Yup.object({
     title: Yup.string().required("El título es requerido"),
+    description: Yup.string().required("La descripción es requerida"),
     state: Yup.string().required("El estado es requerido"),
+    image: Yup.string().required("La imagen es requerida"),
   });
 
   const onSubmit = async (values: UpdateSubsectionHeader) => {
+    const imageUrl = imageFile ? await uploadImage(imageFile) : "";
+
+    if (imageUrl) {
+      values.image = imageUrl;
+    }
+
     await updateSubsectionById(id, values);
   };
+
+  const onChangeImage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.currentTarget.files?.[0];
+    if (file) {
+      const fileSizeMB = file.size / (1024 * 1024);
+      if (fileSizeMB > 30) {
+        setImageFile(null);
+      } else {
+        setImageFile(file);
+      }
+    }
+  };
+
+  const errorOnPreviewImage = () => {
+
+    setPreviewUrl("");
+  }
 
   return (
     <div className="bg-cas-gray-light sm:p-5 flex flex-col justify-center items-center rounded shadow-cas-gray-light drop-shadow w-2/3 sm:w-3/5">
@@ -82,6 +114,25 @@ export const FormSubsectionHeader = ({
                 name="description"
                 component="div"
                 className="text-red-500 text-sm"
+              />
+            </div>
+
+            <div className="flex flex-col pb-4 px-4 rounded w-full">
+              <label>
+                Selecciona una imagen para la cabecera de la subsección
+              </label>
+              <input
+                type="file"
+                accept="image/*"
+                onChange={onChangeImage}
+                className="bg-cas-white p-2 mb-2 border-cas-gray-mid border-[0.5px] rounded w-full"
+              />
+              <Image
+                src={previewUrl || defaultImage}
+                alt="Vista previa"
+                width={1920}
+                height={300}
+                onError={errorOnPreviewImage}
               />
             </div>
 
