0c3a6fb116a0b2654efe82360d272ad406bb592e
Gianpaul Bernal <gianpaulbernalsaravia@gmail.com>
2024-11-30 21:00:52 -0500

    fix: se corrigieron estilos y nombres

--- app/components/FormContactUs.tsx
+++ app/components/FormContactUs.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import { getContacts } from '../api/contactUs';
-import { ContactUs } from '../models';
+import React, { useEffect, useState } from "react";
+import { getContacts } from "../api/contactUs";
+import { ContactUs } from "../models";
 
 export const FormContactUs = () => {
   const [contacts, setContacts] = useState<ContactUs[]>([]);
@@ -9,11 +9,11 @@ export const FormContactUs = () => {
 
   const fetchContacts = async (currentPage: number) => {
     try {
-      const { results, pagination } = await getContacts(currentPage, 10);
+      const { results, pagination } = await getContacts(currentPage, 5);
       setContacts(results);
       setTotalPages(pagination.totalPages);
     } catch (error) {
-      console.error('Error fetching contacts:', error);
+      console.error("Error fetching contacts:", error);
     }
   };
 
@@ -30,12 +30,14 @@ export const FormContactUs = () => {
   };
 
   return (
-    <div>
+    <div className="w-2/3 sm:w-3/5">
       <div className="flex flex-col md:flex-row justify-between items-center bg-cas-gray-light p-2 py-5 rounded-t-lg">
-        <h1 className="text-lg font-semibold px-4">Formulario de Contáctenos</h1>
+        <h1 className="text-lg font-semibold px-4">
+          Lista de mensajes de contacto
+        </h1>
       </div>
 
-      <div className="h-[32rem] w-[84rem] overflow-y-auto border border-cas-gray-light rounded-b-lg">
+      <div className="overflow-y-auto border border-cas-gray-light rounded-b-lg">
         <div className="flex bg-gray-100 font-semibold border-b border-cas-gray-light">
           <div className="w-3/12 px-6 py-3">Fecha</div>
           <div className="w-4/12 px-6 py-3">Nombres</div>
@@ -49,7 +51,7 @@ export const FormContactUs = () => {
               {new Date(contact.publishDate).toLocaleString()}
             </div>
             <div className="w-4/12 px-6 py-3">
-              {contact.names + ' ' + contact.lastNames}
+              {contact.names + " " + contact.lastNames}
             </div>
             <div className="w-5/12 px-6 py-3">{contact.email}</div>
             <div className="w-2/12 px-6 py-3">{contact.cellphone}</div>
@@ -57,19 +59,16 @@ export const FormContactUs = () => {
           </div>
         ))}
         {contacts.length === 0 && (
-          <div className="text-center py-6">
-            No hay contactos disponibles
-          </div>
+          <div className="text-center py-6">No hay contactos disponibles</div>
         )}
       </div>
 
-
       <div className="flex justify-between items-center mt-4">
         <button
-          className={`py-3 px-4 min-w-32 text-[14px] rounded-lg hover:shadow-md hover:opacity-90 ${
+          className={`py-3 px-4 min-w-32 text-[14px] rounded-lg ${
             page === 1
-              ? 'bg-cas-gray-light text-gray-400 cursor-not-allowed'
-              : 'bg-cas-green text-cas-white'
+              ? "bg-cas-gray-light text-gray-400"
+              : "bg-cas-green text-cas-white hover:shadow-md hover:opacity-90"
           }`}
           onClick={handlePrevious}
           disabled={page === 1}
@@ -80,10 +79,10 @@ export const FormContactUs = () => {
           Página {page} de {totalPages}
         </span>
         <button
-          className={`py-3 px-4 min-w-32 text-[14px] rounded-lg hover:shadow-md hover:opacity-90 ${
+          className={`py-3 px-4 min-w-32 text-[14px] rounded-lg ${
             page === totalPages
-              ? 'bg-cas-gray-light text-gray-400 cursor-not-allowed'
-              : 'bg-cas-green text-cas-white'
+              ? "bg-cas-gray-light text-gray-400"
+              : "bg-cas-green text-cas-white hover:shadow-md hover:opacity-90"
           }`}
           onClick={handleNext}
           disabled={page === totalPages}
